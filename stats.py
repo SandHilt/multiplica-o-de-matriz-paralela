9116dccb1386dae6d39e54973499d0f4e964b096
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-01 21:46:48 -0300

    Fazendo o script ler os dados

--- stats.py
+++ stats.py
@@ -1,29 +1,24 @@
 #! python3
 from statistics import mean
-from random import uniform
-from matplotlib import pyplot as plt
+from matplotlib import use, pyplot as plt
+import numpy as np
 
-f = open('matriz/saida.dat', 'r')
+f = open('omp/matriz/saida.dat', 'r')
+
+data = []
 
 # Mostando o que tem no arquivo
 for x in f.readlines():
-    print(x)
+    y = float(x)
+    data.append(y)
+
+plt.boxplot(data)
+plt.show()
 
 f.close()
 
-reps = 3
-tempo_max = 4
-processadores = 2
-tempos_paralelos = []
+# speedup = tempo_sequencial / mean(tempos_paralelos)
+# eficiencia = speedup / processadores * 100
 
-tempo_sequencial = uniform(tempo_max * .75, tempo_max)
-
-for i in range(reps):
-    tempo_paralelo = uniform(tempo_max / processadores, .75 * tempo_max)
-    tempos_paralelos.append(tempo_paralelo)
-    
-speedup = tempo_sequencial / mean(tempos_paralelos)
-eficiencia = speedup / processadores * 100
-
-print('speedup', speedup)
-print('eficiencia', eficiencia)
+# print('speedup', speedup)
+# print('eficiencia', eficiencia)
