d6e64686771ad0172e166de29eba251e067e0b85
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-03 01:39:57 -0300

    Mais alterações no mpi

--- mpi/matriz/paralelo.c
+++ mpi/matriz/paralelo.c
@@ -1,14 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 
-void main(int argc, char **argv) {
-    int rank, size;
+#define SIZE 4			/* Max Size of matrices */
 
-    MPI_Init(&argc, &argv);
-    
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+int A[SIZE][SIZE], B[SIZE][SIZE], C[SIZE][SIZE];
 
-    printf("Process %d of %d is alive\n", rank, size);
-    MPI_Finalize();
+void fill_matrix(int m[SIZE][SIZE])
+{
+  static int n=0;
+  int i, j;
+  for (i=0; i<SIZE; i++)
+    for (j=0; j<SIZE; j++)
+      m[i][j] = n++;
+}
+
+void print_matrix(int m[SIZE][SIZE])
+{
+  int i, j = 0;
+  for (i=0; i<SIZE; i++) {
+    printf("\n\t| ");
+    for (j=0; j<SIZE; j++)
+      printf("%2d ", m[i][j]);
+    printf("|");
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+  int numtasks, rank, dest, source, tag = 1;
+  int inmsg, outmsg;
+
+  double start, end;
+
+  MPI_Status State;
+
+  FILE *saida = fopen('./saida.dat', 'w');
+
+  fill_matrix(A);
+  fill_matrix(B);
+
+  for (int i=0; i<SIZE; i++) 
+    for (int j=0; j<SIZE; j++) {
+      MPI_Init(&argc, &argv);
+
+      MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+      if(rank == 0) {
+        int sum = 0;
+        
+        source = MPI_ANY_SOURCE;
+        tag = MPI_ANY_TAG;
+
+        start = MPI_Wtime();
+        for(int i = 0; i < numtasks - 1; i++){
+
+        }
+      } else {
+        dest = 0;
+        tag = rank;
+
+        for(int i = 0; i < SIZE; i++) {
+
+        }
+      }
+      if(rank == 0){
+        end = MPI_Wtime();
+        fprintf(saida, "%.5f", end - start);
+      }
+
+      // for (int k=0; k<SIZE; k++)
+	    //   sum += A[i][k]*B[k][j];
+
+      // C[i][j]=sum;
+
+      MPI_Finalize();
+    }
+
+  fclose(saida);
+  print_matrix(C);
+
+  return 0;
 }
