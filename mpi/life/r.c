286b180dc423b6ceaed846652441e2f3b8883e71
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-09 17:21:09 -0300

    Teste

--- mpi/life/r.c
+++ mpi/life/r.c
@@ -8,29 +8,29 @@
 int main(int argc, char *argv[])
 {
     int rank, size, i;
-    double *param, *sub_param;
+    int *param, *sub_param;
     srand(time(NULL));
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    param = (double *)malloc(size * SIZE * sizeof(double));
+    param = (int *)malloc(size * SIZE * sizeof(int));
     if (rank == 0)
     {
         for (i = 0; i < size * SIZE; ++i)
         {
-            param[i] = rand() % 2;
+            param[i] = i;
         }
     }
     else
     {
-        sub_param = (double *)calloc(SIZE, sizeof(double));
+        sub_param = (int *)calloc(SIZE, sizeof(int));
     }
 
-    MPI_Scatter(param, SIZE, MPI_DOUBLE, sub_param, SIZE, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-    printf("P:%d sub_param is: ", rank);
+    MPI_Scatter(param, SIZE, MPI_INT, sub_param, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+    printf("P:%d sub_param is:", rank);
     for (i = 0; i < SIZE; i++)
-        printf("%c ", sub_param[i] ? 'x' : '0');
+        printf(" %2d", sub_param[i]);
     printf("\n");
     MPI_Finalize();
     return 0;
