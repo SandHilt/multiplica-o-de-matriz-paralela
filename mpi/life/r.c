ca18f33cd8c8a8e2d7129ea3330e01cccd85d5bd
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-09 19:13:05 -0300

    Mais melhorias para teste

--- mpi/life/r.c
+++ mpi/life/r.c
@@ -3,37 +3,35 @@
 #include <mpi.h>
 #include <time.h>
 
-#define SIZE 10
+#define SIZE 2
 
 int main(int argc, char *argv[])
 {
-    int rank, size, i, j;
+    int rank, size, i, j, info;
     int param[SIZE][SIZE], *sub_param;
     srand(time(NULL));
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    info = SIZE * SIZE / size;
     if (rank == 0)
     {
         for (i = 0; i < SIZE; i++)
-        {
             for (j = 0; j < SIZE; j++)
             {
                 param[i][j] = 10 * i + j;
-                printf("B[%d,%d]=%d\n", i, j, param[i][j]);
             }
-            printf("\n");
-        }
     }
     else
     {
-        sub_param = (int *)calloc(SIZE, sizeof(int));
+        sub_param = (int *)calloc(info, sizeof(int));
     }
 
-    MPI_Scatter(param, SIZE, MPI_INT, sub_param, SIZE, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(param, info, MPI_INT, sub_param, info, MPI_INT, 0, MPI_COMM_WORLD);
     printf("P:%d sub_param is:", rank);
-    for (i = 0; i < SIZE; i++)
+    for (i = 0; i < info; i++)
         printf(" %2d", sub_param[i]);
     printf("\n");
     MPI_Finalize();
