09d7a5c65cb9b0c25e62131addd01b2d1d295d77
Bruno Ochotorena <hilt@outlook.com.br>
2019-12-09 22:16:32 -0300

    Resolvido questao da divisao

--- mpi/life/r.c
+++ mpi/life/r.c
@@ -3,14 +3,7 @@
 #include <mpi.h>
 #include <time.h>
 
-#define SIZE 10
-
-void alloc(int **p, int size)
-{
-    p = (int **)malloc(size * sizeof(int *));
-    for (int i = 0; i < size; i++)
-        p[i] = (int *)calloc(size, sizeof(int));
-}
+#define SIZE 4
 
 int main(int argc, char *argv[])
 {
@@ -25,30 +18,33 @@ int main(int argc, char *argv[])
     info = SIZE / numtasks;
 
     param = (int **)calloc(SIZE, sizeof(int *));
-    sub_param = (int **)calloc(info, sizeof(int *));
+    sub_param = (int **)calloc(SIZE, sizeof(int *));
 
     for (i = 0; i < SIZE; i++)
     {
         param[i] = (int *)calloc(SIZE, sizeof(int));
         sub_param[i] = (int *)calloc(SIZE, sizeof(int));
-    }
 
-    if (rank == 0)
-    {
-        for (i = 0; i < SIZE; i++)
+        if (rank == 0)
+        {
             for (j = 0; j < SIZE; j++)
             {
-                param[i][j] = i + j * SIZE;
+                param[i][j] = j + i * SIZE;
+                printf(" %2d", param[i][j]);
             }
+            printf("\n");
+        }
     }
 
-    for (i = 0; i < info; i++)
+    for (i = 0; i < SIZE; i++)
         MPI_Scatter(param[i], info, MPI_INT, sub_param[i], info, MPI_INT, 0, MPI_COMM_WORLD);
 
-    printf("P:%d sub_param is:", rank);
-    for (i = 0; i < info; i++)
+    printf("rank %d sub_param is:", rank);
+    for (i = 0; i < SIZE; i++)
+    {
         for (j = 0; j < info; j++)
             printf(" %2d", sub_param[i][j]);
+    }
     printf("\n");
     MPI_Finalize();
     return 0;
